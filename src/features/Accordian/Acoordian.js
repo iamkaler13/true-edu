bc2b1bc9150457921901b0686052d4b3e91fc82f
viveks5898 <singh.vivek5898@gmail.com>
2023-09-23 22:55:13 -0700

    design changes

--- src/features/Accordian/Acoordian.js
+++ src/features/Accordian/Acoordian.js
@@ -9,10 +9,20 @@ import {
 import "react-accessible-accordion/dist/fancy-example.css";
 import { Link } from "react-router-dom";
 const Accordian = ({ setShow, nav_links }) => {
-  console.log("nav_links", nav_links)
+  console.log("nav_links", nav_links);
   return (
-    <div className="nav-accordian h-[100vh] overflow-y-scroll mobileview">
-      <Accordion allowZeroExpanded>
+    <div className="nav-accordian h-[100vh] overflow-y-scroll mobileview flex flex-col gap-[20px] pt-[20px]">
+      {nav_links.map((element, key) => (
+        <div className="border-b-[2px] border-[#00ffff] pb-[20px]">
+          <h3 className="text-heading text-md font-semibold">
+            <Link to={element.link} onClick={() => setShow(false)}>
+              {element.name}
+            </Link>
+          </h3>
+        </div>
+      ))}
+
+      {/* <Accordion allowZeroExpanded>
         {nav_links.map((element, key) => (
           <AccordionItem key={key}>
             <AccordionItemHeading className="mobile_arroww">
@@ -32,8 +42,7 @@ const Accordian = ({ setShow, nav_links }) => {
          
           </AccordionItem>
         ))}
-      </Accordion>
-
+      </Accordion> */}
     </div>
   );
 };
