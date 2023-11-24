d218b934a1cf4361904f923e043623e1e265e861
viveks5898 <singh.vivek5898@gmail.com>
2023-11-23 21:21:05 -0800

    fixed css

--- src/features/Booklist/Booklist.js
+++ src/features/Booklist/Booklist.js
@@ -19,14 +19,14 @@ export function ProductList({products}) {
                 </div>
                 <div className="mt-[2px] sm:mt-4  justify-between">
                   <div>
-                  {/* <h2 className={` text-[16px] sm:text-[20px] !font-bold text-black`}>
+                  <h2 className={`hidden sm:block text-[16px] sm:text-[20px] !font-bold text-black`}>
                     <div>{product.name}</div>
                   </h2>
-                  <h3 className={` text-start mb-0 m:mb-0 mt-0 sm:mt-0 text-sm   sm:font-semibold text-heading`}>
+                  <h3 className={` text-start hidden sm:block mb-0 m:mb-0 mt-0 sm:mt-0 text-sm   sm:font-semibold text-heading`}>
                     <p className="sm:text-[15px]">By : {product.Author}</p>
-                  </h3> */}
+                  </h3>
 
-                  <button className="text-[#FF5721] hover:text-heading my-3 sm:my-0 text-lg font-semibold dark:focus:ring-yellow-900 rounded-lg">
+                  <button className="text-[#FF5721] float-right sm:float-left hover:text-heading my-3 text-[16px] sm:text-[18px] sm:my-0 text-lg font-semibold dark:focus:ring-yellow-900 rounded-lg">
                     <Link to={`/books/${product.slug}`}> Read more →</Link>
                   </button>
                   </div>
